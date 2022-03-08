3ea3bfeca8e8f0448f68b612b5f55199b4d0f960
JungGyeongmi <gm950715@gmail.com>
2022-03-08 16:27:33 +0900

    ?

--- baekjoon/src/bruteForceEx/Ex1018.java
+++ baekjoon/src/bruteForceEx/Ex1018.java
@@ -9,9 +9,13 @@ public class Ex1018 {
     public static void main(String[] args) throws IOException { 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
 
+        int[][] arr = {
+            
+        }
 
     }
 }
