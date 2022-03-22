43fd944e4e6b7e95508c05d0ed263956a13adb8a
unknown <gm950715@gmail.com>
2022-03-22 16:44:45 +0900

    3월23일 스터디 N과 M 브루트포스 문제

---
+++ baekjoon/src/codeTestColl/bruteForce/Ex15656.java
@@ -0,0 +1,52 @@
+package codeTestColl.bruteForce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Ex15656 {  
+    public static int[] arr;
+    public static int[] result;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+   
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        result=new int[N];
+        st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        int i = 0;
+        while(st.hasMoreTokens()) {
+            arr[i++]=Integer.parseInt(st.nextToken());
+        }
+        // 오름차순 정렬 
+        Arrays.sort(arr);
+        result = Arrays.copyOf(arr, arr.length);
+
+        dfs(N, M, 0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if(depth == M) {
+            for(int i=0; i<M;i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+           
+            return;
+        }
+
+        for(int i = 0; i < N; i++) {
+            arr[depth]=result[i];
+            dfs(N, M, depth+1);
+        }
+    }
+}
---
+++ baekjoon/src/codeTestColl/bruteForce/Ex18290.java
@@ -0,0 +1,9 @@
+package codeTestColl.bruteForce;
+
+import java.io.IOException;
+
+public class Ex18290 {
+    public static void main(String[] args) throws IOException {
+        
+    }
+}
